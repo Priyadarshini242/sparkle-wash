b4c3a686e058b1f20d5acc054b1b0ea6e744be78
zienc <some>
2025-09-03 16:05:02 +0530

    login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,99 +1,90 @@
-import { useState } from 'react';
-import { Link, useNavigate } from "react-router-dom";
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import carImage from './assets/cropped-car-wash.png';
+// import earningsBarChart from './components/EarningsBarChart';
 
 
 function Login() {
-  const [count, setCount] = useState(0);
-  const navigate = useNavigate();
+  const [count, setCount] = useState(0)
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // You can add validation here if needed
-    navigate("/dashboard");
-  };
- return (
-  
-  <div className='flex'>
-   
-  <div className="flex flex-col justify-center items-center">
-   {/* Left Side - Blue */}
-     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 80" width="110" height="60" aria-labelledby="dropTitle" role="img">
-      <title id="dropTitle">White Water Drop</title>
-      <path d="M32 4  C42 18, 56 34, 56 50  C56 68, 45 78, 32 78  C19 78, 8 68, 8 50 C8 34, 22 18, 32 4 Z" fill="white"/>
-      </svg>
-     <header className="w-full max-w-xl">
-        <h1 className="text-5xl md:text-6xl font-extrabold text-blue-600 heading-shadow" >
-          Sparkle Wash
-        </h1>
-        <img 
-        src="/public/WhatsApp Image 2025-08-28 at 11.08.20 AM.jpeg "
-        alt="Car"
-        className="w-10 h-30 rounded-2xl shadow-lg object-cover bg-gray-100 flex items-center -center"/>
-        <p className="mt-2 text-gray-600">Prefessional Car Washing Serives</p>
-        <h4 class name="text-5xl md:text-6xl font-extrabold text-blue-500 heading-shadow">"SparkelWash alwaysleaves my vehicles looking brand new .the staff is professional and trough!"</h4>
-      </header>
+  return (
+    <div className="flex h-screen w-screen overflow-hidden">
+      {/* Blue container */}
+      <div className="flex flex-col justify-center items-center w-1/2 h-full bg-blue-600 p-0 m-0">
+        <svg class="w-6 h-6 text-white" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+          <path d="M12 2 C9 6.2 6 9.6 6 13.2C6 16.9 8.9 20 12.5 20C16.1 20 19 16.9 19 13.2C19 9.6 15 6.2 12 2Z"/>
+         </svg>
+        <h1 className="text-xxl font-bold text-white text-center mb-3">SparkleWash</h1>
+        <img className="w-full h-48 object-cover mb-4 p-0 m-0" src={carImage} alt="Car Wash" />
+        <p className="text-3xl text-white p-4">Professional Car and Bike Washing Services</p>
+        <div className="bg-indigo-600 rounded-lg shadow-lg p-4 m-4">
+          <p className="card font-(family-name:<custom-property>)font-light text-white p-0">sparkleWash always leaves my vehicles looking brand new. The staff is professional and thorough!</p>
+           <p className='text-white text-content-end'>-Satisfied Customer</p>
+        </div>
+      </div>
+
+      {/* White container */}
+      <div className="w-1/2 h-full bg-white flex flex-col justify-center items-center p-0 m-0">
+        <div className="flex flex-col items-center w-full max-w-sm px-6 py-12">
+          <h2 className="text-xl font-bold tracking-tight text-black mb-2">Admin Login</h2>
+          <p className="text-grey font-normal mb-6">Enter your Credentials to access the admin dashboard</p>
+          <form className="space-y-6 w-full">
+            <div>
+                <div class="flex items-center border rounded-lg px-3 py-2 bg-white">
+                {/* <!-- Email icon --> */}
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    stroke-width="1.5" stroke="currentColor" 
+                    class="w-5 h-5 text-gray-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 7.5l-9 6-9-6" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75l8.88 5.55c.21.13.48.13.69 0l8.88-5.55" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5A2.25 2.25 0 0119.5 19.5H4.5a2.25 2.25 0 01-2.25-2.25V6.75" />
+                </svg>
+                <input type="email" placeholder="Enter your email"
+                      class="ml-2 w-full outline-none border-none" />
+              </div>
+            </div>
+                <div class="flex items-center border rounded-lg px-3 py-2 mt-3 bg-white">
+               {/* <!-- Password input --> */}
+                <input type="password" placeholder="Enter your password"
+                      class="w-full outline-none border-none" />
+                {/* <!-- Eye icon --> */}
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    stroke-width="1.5" stroke="currentColor" 
+                    class="w-5 h-5 text-gray-500 cursor-pointer">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12c2.25-4.5 6.75-7.5 9.75-7.5s7.5 3 9.75 7.5c-2.25 4.5-6.75 7.5-9.75 7.5s-7.5-3-9.75-7.5z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                </svg>
+               </div>
+               <div className="bg-white p-6 rounded-2xl shadow-lg w-full max-w-sm">
+                <div className="flex items-center space-x-2 mb-2">
+                  <input
+                    id="updates"
+                    type="checkbox"
+                    className="h-5 w-5 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                  />
+                  <label htmlFor="updates" className="text-gray-700 justify-between">Remember me</label>
+                  <a href="#" className=" text-sm font-semibold text-indigo-400 hover:text-indigo-300">
+                    Forgot passwor.....
+                  </a>
+                </div>
+              </div>
+              <div>
+                <Link
+                  to="/DashBoard"
+                  className="px-4 py-2 bg-amber-400  text-black rounded-lg inline-block text-center w-full"
+                   >
+                  Login
+                </Link>
+              </div>
+          </form>
+        </div>
+      </div>
     </div>
-   
-
-       {/* Right Side - White */}
-    <div className="w-1/2 h-full flex flex-col justify-center items-center p-0 m-0">
-       <div className="flex flex-col  justify-center item-center w-0/2 h-full bg-blue-100 p-0 m-0">
-           <h2 className="mt-10 text-center text-2xl/9font-bold tracking-tight text-white">Admin Login</h2>
-            <p> Enter your credentials to access the admin dashboard</p>
-         </div>
-             
-
-    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form onSubmit={handleLogin} className="space-y-6">
-      <div>
-        <label htmlFor="Admin" placeholder="Enter your admin Id" className="block text-sm/6 font-medium text-gray-100">Admin ID</label>
-        <div className="mt-2">
-          <input 
-          id="email" 
-          type="email" 
-          name="email"
-          autoComplete="email" 
-          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Enter your email"></input>
-        </div>
-        </div>
-
-       <div>
-         <div className="flex items-center justify-between">
-           <label htmlFor="password" placeholder="Enter your password" className="block text-sm/6 font-medium text-gray-100">Password</label>
-           <div className="text-sm">
-            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
-          </div>
-        </div>
-        <div className="mt-2">
-          <input 
-          id="password" 
-          type="password" 
-          name="password" 
-          autoComplete="current-password" 
-          className="W-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-blue-500"
-          placeholder="Enter your Password"></input>
-        </div>
-          </div>
-          <div>
-              <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Login</button>
-          </div>
-            </form>
-            <p className="mt-10 text-center text-sm/6 text-gray-400">
-               <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Secure login with 256-bit encryption</a>
-            </p>
-           
-             </div>
-     </div>
-</div>
-
- 
- ); 
+  )
 }
+
 export default Login;
-
-
-
